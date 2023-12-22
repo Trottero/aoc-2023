e3956deb2bc44c80648840bfb70f6f41c8966342
Trottero <wrlxs@hotmail.com>
2023-12-22 20:58:09 +0100

    p2 WIP

--- 21/21_test.py
+++ 21/21_test.py
@@ -20,13 +20,34 @@ def test_p1(filename, expected):
 
 
 @pytest.mark.parametrize(
-    "filename, expected",
+    "filename, steps, expected",
     [
         pytest.param(
-            "21/test_input.txt", 0, id="Day 21 Part 2 - Test Input",
+            "21/test_input.txt", 6, 16, id="Day 21 Part 2 - Test Input 6",
+        ),
+        pytest.param(
+            "21/test_input.txt", 10, 50, id="Day 21 Part 2 - Test Input 10",
+        ),
+        pytest.param(
+            "21/test_input.txt", 50, 1594, id="Day 21 Part 2 - Test Input 50",
+        ),
+        pytest.param(
+            "21/test_input.txt", 100, 6536, id="Day 21 Part 2 - Test Input 100",
+        ),
+        pytest.param(
+            "21/test_input.txt", 500, 167004, id="Day 21 Part 2 - Test Input 500",
+        ),
+        pytest.param(
+            "21/test_input.txt", 1000, 668697, id="Day 21 Part 2 - Test Input 1000",
+        ),
+        pytest.param(
+            "21/test_input.txt", 5000, 16733044, id="Day 21 Part 2 - Test Input 5000",
         ),
     ],
 )
-def test_p2(filename, expected):
+def test_p2(filename, steps, expected):
+
+    import part2
+    part2.steps = steps
     # Test cases for p2 function
     assert p2(read_file(filename)) == expected
